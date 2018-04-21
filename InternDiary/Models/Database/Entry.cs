6d277a65f5ee201ff3fbadc7cbee3b91acf73c0a
Jafca <Jafca@users.noreply.github.com>
2018-04-21 15:02:24 +0100

    Fixed issue with Date format in the views

--- InternDiary/Models/Database/Entry.cs
+++ InternDiary/Models/Database/Entry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InternDiary.Models.Database
@@ -6,6 +7,8 @@ namespace InternDiary.Models.Database
     [Table("Entries")]
     public class Entry : BaseModel
     {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
         public string Title { get; set; }
