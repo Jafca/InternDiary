8d803d729ab2cfbf194822f02caafdbb70330ed4
Jafca <Jafca@users.noreply.github.com>
2018-04-21 14:31:07 +0100

    Added default controller and view for Entry

---
+++ InternDiary/Controllers/EntryController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InternDiary.Models;
+using InternDiary.Models.Database;
+
+namespace InternDiary.Controllers
+{
+    public class EntryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Entry
+        public ActionResult Index()
+        {
+            return View(db.Entries.ToList());
+        }
+
+        // GET: Entry/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // GET: Entry/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Entry/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Date,Title,Rating,Content")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                entry.Id = Guid.NewGuid();
+                db.Entries.Add(entry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(entry);
+        }
+
+        // GET: Entry/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entry/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Date,Title,Rating,Content")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(entry);
+        }
+
+        // GET: Entry/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entry/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Entry entry = db.Entries.Find(id);
+            db.Entries.Remove(entry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
