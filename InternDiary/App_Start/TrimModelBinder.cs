56c71d6d0b42459885624406bebd8d8e606f7045
Jafca <Jafca@users.noreply.github.com>
2018-05-06 21:29:13 +0100

    Added TrimModelBinder to trim all Form input

---
+++ InternDiary/App_Start/TrimModelBinder.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace InternDiary
+{
+    public class TrimModelBinder : DefaultModelBinder
+    {
+        protected override void SetProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, object value)
+        {
+            if (propertyDescriptor.PropertyType == typeof(string))
+            {
+                var stringValue = (string)value;
+                value = string.IsNullOrEmpty(stringValue) ? stringValue : stringValue.Trim();
+            }
+
+            base.SetProperty(controllerContext, bindingContext, propertyDescriptor, value);
+        }
+    }
+}
