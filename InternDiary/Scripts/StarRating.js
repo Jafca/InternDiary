7d888d726d0732a938088211bca2f89868aa15f7
Jafca <Jafca@users.noreply.github.com>
2018-05-24 00:18:57 +0100

    Fixed null refs in StarRating

--- InternDiary/Scripts/StarRating.js
+++ InternDiary/Scripts/StarRating.js
@@ -1,38 +1,40 @@
 ﻿// Displaying readonly stars
-var ratings = document.body.getElementsByClassName("displayRating");
-for (var i = 0; i < ratings.length; i++) {
-    ratings[i].innerHTML = "\u2605".repeat(ratings[i].innerHTML) + "\u2606".repeat(5 - ratings[i].innerHTML);
+var displayRating = document.getElementById("displayRating");
+if (displayRating !== null) {
+    displayRating.innerHTML = "\u2605".repeat(displayRating.innerHTML) + "\u2606".repeat(5 - displayRating.innerHTML);
 }
-
 // Displaying stars for Create and Edit
-var star1 = document.createElement("span");
-star1.innerHTML = "☆";
-star1.classList.add("btn", "star");
+var starRatingControl = document.getElementById("starRatingControl");
+if (starRatingControl !== null) {
+    var star1 = document.createElement("span");
+    star1.innerHTML = "☆";
+    star1.classList.add("btn", "star");
 
-var star2 = document.createElement("span");
-star2.innerHTML = "☆";
-star2.classList.add("btn", "star");
+    var star2 = document.createElement("span");
+    star2.innerHTML = "☆";
+    star2.classList.add("btn", "star");
 
-var star3 = document.createElement("span");
-star3.innerHTML = "☆";
-star3.classList.add("btn", "star");
+    var star3 = document.createElement("span");
+    star3.innerHTML = "☆";
+    star3.classList.add("btn", "star");
 
-var star4 = document.createElement("span");
-star4.innerHTML = "☆";
-star4.classList.add("btn", "star");
+    var star4 = document.createElement("span");
+    star4.innerHTML = "☆";
+    star4.classList.add("btn", "star");
 
-var star5 = document.createElement("span");
-star5.innerHTML = "☆";
-star5.classList.add("btn", "star");
+    var star5 = document.createElement("span");
+    star5.innerHTML = "☆";
+    star5.classList.add("btn", "star");
 
-document.getElementById("starRatingControl").append(star1, star2, star3, star4, star5);
+    starRatingControl.append(star1, star2, star3, star4, star5);
 
-// Display stars if rating already has a value
-var ratingValue = document.getElementById("ratingValue");
-if (ratingValue.value > 0) {
-    var stars = $(".star");
-    for (var i = 0; i < ratingValue.value; i++) {
-        stars[i].innerHTML = '★';
+    // Display stars if rating already has a value
+    var ratingValue = document.getElementById("ratingValue");
+    if (ratingValue.value > 0) {
+        var stars = $(".star");
+        for (var i = 0; i < ratingValue.value; i++) {
+            stars[i].innerHTML = '★';
+        }
     }
 }
 
